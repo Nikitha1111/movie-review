32061610d41849c9bb239439884d19475cb0e8da
Nikitha1111 <nikithasankuru98.com>
2024-02-29 19:02:28 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {  BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {  HashRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './MovieAppComponents/home/Home';
 import Header from './MovieAppComponents/Header/Header';
 import Footer from './MovieAppComponents/Footer/Footer';
